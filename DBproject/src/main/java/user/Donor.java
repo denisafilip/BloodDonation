eb614b4f8510cf1270c490adde11f7b1c354ee54
denisafilip <denifilip02@yahoo.com>
2020-12-23 20:23:46 +0200

    made Patient and Donor child classes of User

--- DBproject/src/main/java/user/Donor.java
+++ DBproject/src/main/java/user/Donor.java
@@ -1,73 +1,16 @@
 package user;
 
 import java.time.LocalDate;
-import java.time.Period;
 
-public class Donor {
-    private String firstName;
-    private String lastName;
+public class Donor extends User {
+
     private String email;
-    private String phoneNumber;
-    private LocalDate dateOfBirth;
-    private int age;
-    private String gender;
-    private String bloodType;
     private int weight;
-    private String county;
-    private String CNP;
 
-    public Donor(String firstName, String lastName, String email, String phoneNumber, LocalDate dateOfBirth, String gender, String bloodType, int weight, String county, String CNP) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Donor(String firstName, String lastName, String phoneNumber, LocalDate dateOfBirth, String gender, String bloodType, boolean RH, String county, String CNP, String email, int weight) {
+        super(firstName, lastName, phoneNumber, dateOfBirth, gender, bloodType, RH, county, CNP);
         this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.dateOfBirth = dateOfBirth;
-        this.age = computeUserAge();
-        this.gender = gender;
-        this.bloodType = bloodType;
         this.weight = weight;
-        this.county = county;
-        this.CNP = CNP;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getBloodType() {
-        return bloodType;
-    }
-
-    public void setBloodType(String bloodType) {
-        this.bloodType = bloodType;
     }
 
     public String getEmail() {
@@ -84,107 +27,5 @@ public class Donor {
 
     public void setWeight(int weight) {
         this.weight = weight;
-    }
-
-    public String getCounty() {
-        return county;
-    }
-
-    public void setCounty(String county) {
-        this.county = county;
-    }
-
-    public String getCNP() {
-        return CNP;
-    }
-
-    public void setCNP(String CNP) {
-        this.CNP = CNP;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-
-    /**
-     *
-     * @return true if donor's CNP is valid and meets all of the requirements
-     */
-    public boolean verifyCNP() {
-        String constant = "279146358279";
-        if (!this.CNP.matches("[0-9]+")) {
-            System.err.println("You must enter only numbers for the CNP");
-            return false;
-        }
-        if (this.CNP.length() != 13) {
-            System.err.println("The CNP must have 13 digits.");
-            return false;
-        }
-        int sum = 0;
-        for (int i = 0; i < constant.length(); i++) {
-            sum = sum + this.CNP.charAt(i) * constant.charAt(i);
-        }
-        int rest = sum % 11;
-        int controlDigit = Character.getNumericValue(this.CNP.charAt(12));
-        if ((rest < 10 && rest == controlDigit) || (rest == 10 && controlDigit == 1)) {
-            if ((this.gender.equals("F") && (this.CNP.charAt(0) == '6' || this.CNP.charAt(0) == '2')) ||
-                    (this.gender.equals("M") && (this.CNP.charAt(0) == '5' || this.CNP.charAt(0) == '1'))) {
-                return true;
-            } else {
-                System.err.println("The first digit of your CNP does not match your gender.");
-                return false;
-            }
-        } else {
-            System.err.println("The CNP you entered is not valid.");
-            return false;
-        }
-    }
-
-    /**
-     *
-     * @return user's age, computed as a difference of the current date and the user's date of birth
-     */
-    public int computeUserAge() {
-        LocalDate now = LocalDate.now();
-        Period difference = Period.between(this.dateOfBirth, now);
-        return difference.getYears();
-    }
-
-    /**
-     *
-     * @return true if user is over 18 and under 110 years old (eligible to donate blood)
-     */
-    public boolean isEligibleToDonate() {
-        return (this.age >= 18 && this.age < 65);
-    }
-
-
-    /**
-     *
-     * @return true if user's first name is formed only of letters, spaces and hyphens
-     */
-    public boolean verifyFirstName() {
-        return (this.firstName.matches("[a-zA-Z- ]+"));
-    }
-
-    /**
-     *
-     * @return true if user's last name is formed only of letters, spaces and hyphens
-     */
-    public boolean verifyLastName() {
-        return (this.lastName.matches("[a-zA-Z- ]+"));
     }
 }
--- DBproject/src/main/java/user/Patient.java
+++ DBproject/src/main/java/user/Patient.java
@@ -1,94 +1,15 @@
 package user;
 
-public class Patient {
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private int age;
-    private String gender;
-    private String bloodType;
-    private Boolean rh;
-    private String county;
-    private String CNP;
+import java.time.LocalDate;
+
+public class Patient extends User {
+
     private String hospital;
-    private Boolean recieved;
+    private Boolean received;
 
-    public Patient(String firstName, String lastName,String gender, String phoneNumber, int age, String bloodType, Boolean rh, String county, String CNP, String hospital, Boolean recieved) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.age = age;
-        this.bloodType = bloodType;
-        this.rh = rh;
-        this.county = county;
-        this.CNP = CNP;
+    public Patient(String firstName, String lastName, String phoneNumber, LocalDate dateOfBirth, String gender, String bloodType, boolean RH, String county, String CNP, String hospital) {
+        super(firstName, lastName, phoneNumber, dateOfBirth, gender, bloodType, RH, county, CNP);
         this.hospital = hospital;
-        this.recieved = recieved;
-        this.gender = gender;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getBloodType() {
-        return bloodType;
-    }
-
-    public void setBloodType(String bloodType) {
-        this.bloodType = bloodType;
-    }
-
-    public Boolean getRh() {
-        return rh;
-    }
-
-    public void setRh(Boolean rh) {
-        this.rh = rh;
-    }
-
-    public String getCounty() {
-        return county;
-    }
-
-    public void setCounty(String county) {
-        this.county = county;
-    }
-
-    public String getCnp() {
-        return CNP;
-    }
-
-    public void setCnp(String CNP) {
-        this.CNP = CNP;
     }
 
     public String getHospital() {
@@ -99,89 +20,11 @@ public class Patient {
         this.hospital = hospital;
     }
 
-    public Boolean getRecieved() {
-        return recieved;
+    public Boolean getReceived() {
+        return received;
     }
 
-    public void setRecieved(Boolean recieved) {
-        this.recieved = recieved;
+    public void setReceived(Boolean recieved) {
+        this.received = recieved;
     }
-
-    public boolean verifyFirstName() {
-        return (this.firstName.matches("[a-zA-Z- ]+"));
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    /**
-     *
-     * @return true if user's last name is formed only of letters, spaces and hyphens
-     */
-    public boolean verifyLastName() {
-        return (this.lastName.matches("[a-zA-Z- ]+"));
-    }
-
-    public boolean verifyCNP() {
-        String constant = "279146358279";
-        if (!this.CNP.matches("[0-9]+")) {
-            System.err.println("You must enter only numbers for the CNP");
-            return false;
-        }
-        if (this.CNP.length() != 13) {
-            System.err.println("The CNP must have 13 digits.");
-            return false;
-        }
-        int sum = 0;
-        for (int i = 0; i < constant.length(); i++) {
-            sum = sum + this.CNP.charAt(i) * constant.charAt(i);
-        }
-        int rest = sum % 11;
-        int controlDigit = Character.getNumericValue(this.CNP.charAt(12));
-        if ((rest < 10 && rest == controlDigit) || (rest == 10 && controlDigit == 1)) {
-            if ((this.gender.equals("F") && (this.CNP.charAt(0) == '6' || this.CNP.charAt(0) == '2')) ||
-                    (this.gender.equals("M") && (this.CNP.charAt(0) == '5' || this.CNP.charAt(0) == '1'))) {
-                return true;
-            } else {
-                System.err.println("The first digit of your CNP does not match your gender.");
-                return false;
-            }
-        } else {
-            System.err.println("The CNP you entered is not valid.");
-            return false;
-        }
-    }
-
-    /**
-     * @return true if the phone number contains only digits
-     */
-    public Boolean verifyPhoneNumberr(){
-        if(this.phoneNumber.matches("[0-9]+") && this.phoneNumber.length() == 10){
-            return true;
-        }
-        else{
-            System.err.println("The phone number must contain only digits and have the length equals to 10.");
-            return false;
-        }
-    }
-
-    /**
-     * @returns true if the blood type is valid
-     */
-
-    public Boolean verifyBloodType(){
-        if(this.bloodType.equals("01") || this.bloodType.equals("A2") || this.bloodType.equals("B3") || this.bloodType.equals("AB4")){
-            return true;
-        }
-        else {
-            System.err.println("The blood type is incorrect.");
-            return false;
-        }
-    }
-
 }
